b62d5288ed0be4dd968e75387ef03765ed3446eb
Yamparala Devesh <dev344@gmail.com>
2013-08-28 12:19:39 +0530

    Changed start simulator script

--- start_simulator.sh
+++ start_simulator.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 source /opt/ros/fuerte/setup.bash
 export ROS_PACKAGE_PATH=$ROS_PACKAGE_PATH:~/ros_home/tum_simulator
-roslaunch cvg_sim_test tum_kitchen.launch
+roslaunch cvg_sim_test 3boxes_room.launch
+
