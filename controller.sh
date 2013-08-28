a0547a868ed1515082bd54fc418ab2892f2a5a18
Yamparala Devesh <dev344@gmail.com>
2013-08-28 13:01:54 +0530

    skype change 3

--- controller.sh
+++ controller.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 source /opt/ros/groovy/setup.bash 
 export ROS_PACKAGE_PATH=$ROS_PACKAGE_PATH:~/ros_home
-export ROS_PACKAGE_PATH=$ROS_PACKAGE_PATH:~/ros_home/semi_autonomous_drone/
+export ROS_PACKAGE_PATH=$ROS_PACKAGE_PATH:~/semi_autonomous_drone/
 python event_listener.py
